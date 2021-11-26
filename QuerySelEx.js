2f331eafb66663a3d4479695915d8054a96c0c84
MendezJennifer <77803373+MendezJennifer@users.noreply.github.com>
2021-11-25 21:59:45 -0700

    Q1 & 2 complete
    Changed Q1 a & b to use event listeners

--- QuerySelEx.js
+++ QuerySelEx.js
@@ -1,19 +1,87 @@
-//create a function to change the page background color to fucsia
+//"COMP 1073: Client-Side JavaScript"
+//"Assignment 2"
+//Team 5
+//Members:Jennifer Mendez Chan, Marcos De Gois Borgues, Alex Victor Marques Moreira, Amra Mohamed Akram"
+
+
+//Question 1
+//A)Create a function to change the page background color to fucsia
+
+//Event listener
+document.getElementById("changeBackgroundColor").addEventListener("click", changeBackgroundColor);
+
+//Function to change background color
 function changeBackgroundColor() {
     document.body.style.backgroundColor = "fuchsia";
 }
 
-//create a function to change the color of the h1 element class title to green and change the text to "Welcome to Georgian College"
+//B)Create a function to change the color of the h1 element class title to green and change the text to "Welcome to Georgian College"
+
+//Event listener
+document.getElementById("changeTitle").addEventListener("click", changeTitle);
+
+//Function to change title
 function changeTitle() {
     var title = document.getElementsByClassName("title");
     title[0].style.color = "green";
     title[0].innerHTML = "Welcome to Georgian College";
 }
 
-function insertCourses(){
+//C)Inside “<div id="course-list">” create and insert two ordered list displaying two more coursesIOS-1 &IOS-2.
+// You also have to create classes for these courses. The newly added course & delete button should have a 
+//different Text color(yellow), font size (40), font family (Algerian). With both new courses append a text
+//” This a new course”
 
-    var ulFromList = document.querySelector("#course-list ul");
-    console.log(ulFromList);
+	//Add first course
+    var li = document.createElement("li");
+    var courseSpan = document.createElement("span");
+    var deleteButton = document.createElement("span");
+	var ulFromList = document.querySelector("#course-list ul");	
+	
+	//Add text inside elements
+    li.appendChild(courseSpan).innerHTML="IOS-1";
+    li.appendChild(deleteButton).innerHTML="delete";
+	ulFromList.appendChild(li);
+	
+	//Add classes
+	deleteButton.className = "deleteNew";
+    courseSpan.className = "nameNew"
+
+	//Append text
+	li.appendChild(courseSpan).textContent+="This is a new course";
+
+
+
+	//Add second course
+    var li = document.createElement("li");
+    var courseSpan = document.createElement("span");
+    var deleteButton = document.createElement("span");
+	var ulFromList = document.querySelector("#course-list ul");
+	
+	//Add text inside elements
+    li.appendChild(courseSpan).innerHTML="IOS-2";
+    li.appendChild(deleteButton).innerHTML="delete";
+	ulFromList.appendChild(li);
+	
+	//Add classes
+	deleteButton.className = "deleteNew";
+    courseSpan.className = "nameNew"
+
+	//Append text
+	li.appendChild(courseSpan).textContent+="This is a new course";
+	
+	//functioning delete buttons
+	
+
+	//Add second course
+/*  var node = document.createElement("li");
+  var textnode = document.createTextNode("IOS-2");
+  node.appendChild(textnode);
+  document.querySelector("#course-list ul").appendChild(node);
+*/
+
+ // ul.appendChild(li);
+/*    var ulFromList = document.querySelector("#course-list ul");
     //store text inside the textarea in a variable
     var textArea = document.getElementById("course-name").value;
 
@@ -34,13 +102,12 @@ function insertCourses(){
     
     //append a text that says "new course"
     var newCourse = document.createElement("p");
+	newCourse.innerHTML="IOS-1";
     newCourse.textContent = "This Is a New Course";
     newCourse.className = "newCourse";
     document.querySelector("#course-list ul").appendChild(newCourse);
-
-  
-    
-}
+    console.log(ulFromList);
+  */
 
 // var newTitles = document.getElementsByClassName("title");
 // Array.from(newTitles).forEach(function(element){
@@ -60,7 +127,7 @@ function insertCourses(){
 //     console.log(e);
 // });
 
-var getDeleteButtons = 
+/*var getDeleteButtons = 
     document.getElementsByClassName("delete");
     console.log(getDeleteButtons);
     //create an Array from the getDeleteButtons variable
@@ -79,4 +146,21 @@ addForm.addEventListener("submit", function(e){
     e.preventDefault();
     insertCourses();
 });
+*/
 
+
+//Question 2
+//A) Using DOM, add a link (like www.yahoo.com) to the source of text after Paragraph tag in
+//given HTML File(attached).
+	//anchor
+	
+    var a = document.createElement("a");
+	const target = document.querySelector("#changeBackgroundColor");
+	target.parentNode.insertBefore(a, target);
+
+	
+	
+	//Add text and link inside elements
+    a.innerHTML="Click Me";
+	a.setAttribute("href", "https://www.youtube.com/watch?v=QH2-TGUlwu4")
+	a.className="name";
